9f5e4f47a26b25284ad4f3e308a3702a791a07e7
SIMON WACHIRA <122459752+Simonwachira7318@users.noreply.github.com>
2024-07-04 15:32:11 +0300

    updated #autocomplete

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,11 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import tw from 'twrnc'
+import { Icon } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
+
+const data= [
+  {
+    id: "123",
+    title: "Get a ride",
+    Image: "https://simonimageurl.netlify.app/images/uberX.png",
+    screen: "MapScreen"
+
+  },
+  {
+    id: "466",
+    title: "Order Food",
+    Image: "https://simonimageurl.netlify.app/images/rider.png",
+    screen: "EatScreen"
+  }
+]
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList 
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) =>(
+        <TouchableOpacity 
+          onPress={() =>navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 h-60 rounded-md`}>
+            <View>
+            <Image
+              style={{width: 120, height:120, resizeMode: "contain"}}
+              source={{uri: item.Image}}
+            />
+            <Text style={tw`mt-2 text-lg font-extrabold`}>{item.title}</Text>
+            <Icon style={tw`p-1 bg-gray-800 rounded-full w-8 mt-4`} name='arrowright' color="white" type="antdesign"/>
+            </View>
+        </TouchableOpacity>
+      )
+      } 
+    />
   )
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,52 @@
-import { StyleSheet, SafeAreaView, View, Image } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
-import NavOptions from '../components/NavOptions'
+import { StyleSheet, SafeAreaView, View, Image } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full w-full`}>
-      {/* <Text style={tw`text-gray-900 text-lg font-bold`}>HomeScreen</Text> */}
-      <View style={tw`flex-1 p-6`}>
+      <View style={tw`flex-1 p-4`}>
         <Image
-          source={{ uri: "https://simonimageurl.netlify.app/images/UberLogo.png" }}
-          style={[tw`rounded-lg `, { width: 80, height: 40}]} 
+          source={{ uri: 'https://simonimageurl.netlify.app/images/UberLogo.png' }}
+          style={[tw`rounded-lg`, { width: 120, height: 80, resizeMode: 'contain' }]}
         />
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) =>{
+            console.log(data);
+            console.log(details);
+          }
+          }
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+          debounce={400}
+        />
+        <NavOptions />
       </View>
-      
-      <NavOptions/>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-  text: {}
+  text: {},
 });
